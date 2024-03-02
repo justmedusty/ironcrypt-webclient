9c724dfbc3d3c2303eea0e5dbd4e37bece8640b5
Dustyn <dustyn_gibb@hotmail.com>
2024-03-01 20:20:10 -0500

    added enums file

---
+++ src/routes/utils/enums.ts
@@ -0,0 +1,19 @@
+
+export const URI = {
+    BASE_URI: "/ironcrypt",
+    BASE_URL: "http://127.0.0.1",
+    LOGIN: "/login",
+    SIGNUP: "/signup",
+    CHANGE_USERNAME: "/profile/changeUserName",
+    CHANGE_PASSWORD: "/profile/changePassword",
+    DELETE_ACCOUNT: "/profile/deleteAccount",
+    UPLOAD_KEY: "/key/upload",
+    GET_KEY: "/key/getMyPublicKey",
+    DELETE_KEY: "/key/deleteMyPublicKey",
+    FILE_UPLOAD: "/file/upload",
+    FILE_DOWNLOAD: "/file/download",
+    FILE_DELETE: "/file/delete"
+
+
+
+}
