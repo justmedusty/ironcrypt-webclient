db507f68456a3086b3e19afff72ded70f382d4f1
Dustyn <dustyn_gibb@hotmail.com>
2024-03-06 16:30:03 -0500

    Changed about page

--- src/routes/components/About.svelte
+++ src/routes/components/About.svelte
@@ -2,14 +2,18 @@
     <h1>About this app</h1>
 
     <p>
-        This is an PGP centric file storage web app created with ktor and svelte. I created this after my previous encrypted messaging service Encryptix, which utilized ktor as
-        well for the backend and react for the front end. You can create your own svelte app with the following terminal command from a project directory.
+        This is an PGP centric file storage web app created with ktor and svelte. I created this after my previous
+        encrypted messaging service Encryptix, which utilized ktor as
+        well for the backend and react for the front end. You can access Encryptix here :
     </p>
-
-    <pre>npm create svelte@latest</pre>
-
+    <a href="http://fxrs5p2pcw47hy6y7o2tgzanmms4ozt7fgfhhtjc2bffliy6ahzmqkqd.onion" style="color: blue">Encryptix</a>
+    <br>
+    <br/>
+    <a href="http://rfocekyg734pio6djwunr4j6a7kdutgzux7mm5npy33azjwcocfslkad.onion" style="color: blue">Mirror</a>
     <p>
-        The way this service works is my servers host files and messages however they are automatically encrypted with your public key, meaning that once they are stored they cannot be decrypted
-        except by the person they were intended for, allowing for a very strong sense (and reality) of privacy. I enjoy building privacy centric applications and will continue to do so in my quest to learn and build things.
+        The way this service works is my servers host files and messages however they are automatically encrypted with
+        your public key, meaning that once they are stored they cannot be decrypted
+        except by the person they were intended for, allowing for a very strong sense (and reality) of privacy. I enjoy
+        building privacy centric applications and will continue to do so in my quest to learn and build things.
     </p>
 </div>
